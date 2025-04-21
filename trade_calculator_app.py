a50dcf34a194525e24ab18aeea512b17287e0c24
Redwards <133527764+Redwards2@users.noreply.github.com>
2025-04-21 12:09:38 -0400

    Update trade_calculator_app.py

--- trade_calculator_app.py
+++ trade_calculator_app.py
@@ -90,7 +90,6 @@ st.sidebar.header("Import Your League")
 username = st.sidebar.text_input("Enter your Sleeper username").strip()
 username_lower = username.lower()
 
-
 league_id = None
 league_options = {}
 df = pd.DataFrame()
@@ -168,7 +167,7 @@ if username:
                     st.markdown("No good 1-for-1 trade suggestions found.")
 
                 st.subheader("2-for-1 Trade Suggestions")
-player_filter_name = st.text_input("Filter 2-for-1 Trades by Player Name (Player 1 OR 2)", key="player_filter").strip().lower()
+                player_filter_name = st.text_input("Filter 2-for-1 Trades by Player Name (Player 1 OR 2)", key="player_filter").strip().lower()
                 two_low = int(adjusted_value * (1 - tolerance / 100))
                 two_high = int(adjusted_value * (1 + tolerance / 100))
 
