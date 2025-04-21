5ba77f8456114f76ef1e2bbf7b775ee25973c29f
Redwards <133527764+Redwards2@users.noreply.github.com>
2025-04-21 12:05:44 -0400

    Update trade_calculator_app.py

--- trade_calculator_app.py
+++ trade_calculator_app.py
@@ -169,7 +169,6 @@ if username:
 
                 st.subheader("2-for-1 Trade Suggestions")
 player_filter_name = st.text_input("Filter 2-for-1 Trades by Player Name (Player 1 OR 2)", key="player_filter").strip().lower()
-player_filter_name = st.text_input("Filter 2-for-1 Trades by Player Name (Player 1 OR 2)", key="player_filter").strip().lower()
                 two_low = int(adjusted_value * (1 - tolerance / 100))
                 two_high = int(adjusted_value * (1 + tolerance / 100))
 
